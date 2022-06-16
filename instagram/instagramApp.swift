c2f3cbf14a90092e586885a3e16e1a01e0984993
Jaegoomon <jaegoo1199@gmail.com>
2022-06-16 20:21:03 +0900

    first commit

---
+++ instagram/instagramApp.swift
@@ -0,0 +1,18 @@
+//
+//  instagramApp.swift
+//  instagram
+//
+//  Created by 조재구 on 2022/06/16.
+//
+//
+
+import SwiftUI
+
+@main
+struct instagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
